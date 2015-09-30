210a565b09c3b17f16630a00daa0a9f053e1dd99
Gu1 <gu1@aeroxteam.fr>
2015-09-30 15:22:02 +0200

    Forgot to commit index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var functions = require('./functions');
+var importer = require('./importer');
+
+
+module.exports.importer = importer;
+module.exports.resolve_function = functions.resolve_function;
