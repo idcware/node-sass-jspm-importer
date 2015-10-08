6e7eee385344a827085b64a77de30b52e3d3854f
siready <janissf@gmail.com>
2015-10-08 23:23:13 +0200

    Cross platform compatibility (fixes Windows path resolving)

--- src/importer.js
+++ src/importer.js
@@ -3,7 +3,7 @@
 var path = require('path');
 var fs = require('fs');
 var sass = require('./sass');
-var jspm = require('jspm')
+var jspm = require('jspm');
 
 
 module.exports = function(url, prev, done) {
@@ -12,32 +12,32 @@ module.exports = function(url, prev, done) {
 
     url = url.replace(/^jspm:/, '')+'.scss';
 
-    jspm.normalize(url).then(function(path) {
+    jspm.normalize(url).then(function(filePath) {
         var stat;
         var parts;
 
-        path = path.replace(/file:\/\/(.*?)(\.js)?$/, '$1');
+        filePath = path.resolve(filePath.replace(/file:\/{2,3}(.*?)(\.js)?$/, '$1'));
         try {
-            stat = fs.statSync(path);
+            stat = fs.statSync(filePath);
         } catch (e) {
             try {
-                parts = path.split('/');
+                parts = filePath.split(path.sep);
                 parts[parts.length - 1] = '_' + parts[parts.length - 1];
-                path = parts.join('/');
-                stat = fs.statSync(path);
+                filePath = parts.join(path.sep);
+                stat = fs.statSync(filePath);
             } catch (e) {
                 return done();
             }
         }
         if(stat.isFile()) {
             done({
-                file: path
+                file: filePath
             });
         } else {
             done();
         }
     }, function(e) {
-        console.log("Could not resolve path:", url)
+        console.log("Could not resolve path:", url);
         done();
     });
 };
