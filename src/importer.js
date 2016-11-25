e4c89405a9787fec1b4f6d5d9fa36a1b45113361
Guillaume Barthelemy <gbarthelemy@solutionsmedias360.ca>
2016-11-25 18:10:57 -0500

    feature add support for sass file and partial
    - refactor the importer internals
    - add support sass file and partial
    - add tests for sass file and partial

--- src/importer.js
+++ src/importer.js
@@ -8,51 +8,68 @@ var fromFileURL = require('./common').fromFileURL;
 
 
 module.exports = function(url, prev, done) {
-    if(url.substr(0, 5) != 'jspm:')
+    if (url.substr(0, 5) != 'jspm:')
         return done(); // bailout
 
-    url = url.replace(/^jspm:/, '')+'.scss';
+    url = url.replace(/^jspm:/, '');
 
     jspm.normalize(url).then(function(filePath) {
+        var file = resolveFile(filePath, '.scss');
+        if (file) {
+            done(file);
+        } else {
+            file = resolveFile(filePath, '.sass');
+            if (file) {
+                done(file);
+            } else {
+                file = resolveFile(filePath, '.css')
+                if (file) {
+                  done(file);
+                } else {
+                  done();
+                }
+            }
+        }
+    });
+
+    function resolveFile(filePath, extension) {
         var stat;
         var parts;
         var origFilePath;
+        var regex;
+        var scssRegex = /\.scss$/;
+        var sassRegex = /\.sass$/;
 
-        origFilePath = path.resolve(fromFileURL(filePath).replace(/\.js$/, ''));
+        origFilePath = path.resolve(fromFileURL(filePath).replace(/\.js$/, extension));
         filePath = origFilePath;
 
         try {
             stat = fs.statSync(filePath);
+            if (extension === '.css') {
+                // The file is there, with the .css extension.
+                // Strip the .css from the filePath to have SASS build this into
+                // the output. If we keep the .css extension here, it leaves it
+                // a plain CSS @import for the browser to resolve.
+                filePath = filePath.replace(/\.css$/, '');
+            }
         } catch (e) {
-            try {
-                parts = filePath.split(path.sep);
-                parts[parts.length - 1] = '_' + parts[parts.length - 1];
-                filePath = parts.join(path.sep);
-                stat = fs.statSync(filePath);
-            } catch (e) {
-                // Check if the file exists with a .css extension
-                filePath = origFilePath.replace(/\.scss$/, '.css');
+            if (extension === '.css') {
+              return null;
+            } else {
                 try {
+                    parts = filePath.split(path.sep);
+                    parts[parts.length - 1] = '_' + parts[parts.length - 1];
+                    filePath = parts.join(path.sep);
                     stat = fs.statSync(filePath);
-                    // The file is there, with the .css extension.
-                    // Strip the .css from the filePath to have SASS build this into
-                    // the output. If we keep the .css extension here, it leaves it
-                    // a plain CSS @import for the browser to resolve.
-                    filePath = filePath.replace(/\.css$/, '');
                 } catch (e) {
-                    return done();
+                    return null;
                 }
             }
         }
-        if(stat.isFile()) {
-            done({
-                file: filePath
-            });
+        if (stat.isFile()) {
+            return { file: filePath };
         } else {
-            done();
+            return null;
         }
-    }, function(e) {
-        console.log("Could not resolve path:", url);
-        done();
-    });
+    }
 };
